9b2132dd587977b2de868a49792f1c7f26d39d91
Adam Clark <adam@iris.washington.edu>
2021-08-12 22:04:33 -0700

    Added tests for as_timedelta fix

--- es_common/tests.py
+++ es_common/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from es_common.data_id import join_provenances, create_data_id
+from es_common.templatetags.es_common_tags import as_timedelta
+import datetime
 from django.core.validators import URLValidator
 
 
@@ -30,4 +32,28 @@ class DataProvenanceTest(TestCase):
         self.assertEqual(
             joined,
             ['a', 'b', 'c', 'd', 'c2'],
+        )
+
+
+class AsTimedeltaFilterTest(TestCase):
+    def test_as_timedelta1(self):
+        self.assertEqual(
+            as_timedelta(datetime.timedelta(days=3, hours=4, minutes=2)),
+            '3d:4h'
+        )
+        self.assertEqual(
+            as_timedelta(datetime.timedelta(hours=4, minutes=2, seconds=30)),
+            '4h:2m'
+        )
+        self.assertEqual(
+            as_timedelta(datetime.timedelta(seconds=30, milliseconds=234)),
+            '30s'
+        )
+        self.assertEqual(
+            as_timedelta(datetime.timedelta(seconds=3, milliseconds=234)),
+            '3.23s'
+        )
+        self.assertEqual(
+            as_timedelta(datetime.timedelta(microseconds=87236)),
+            '87.2ms'
         )
